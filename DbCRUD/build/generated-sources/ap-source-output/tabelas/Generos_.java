7e3edd0700d1cfe36aa073ca1b9c4f6995c74048
Tomás Abril <tomasabril@users.noreply.github.com>
2015-11-26 22:12:08 -0200

    criei janela
    ta vazia ainda

--- DbCRUD/build/generated-sources/ap-source-output/tabelas/Generos_.java
+++ DbCRUD/build/generated-sources/ap-source-output/tabelas/Generos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-26T22:05:46")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-26T22:10:34")
 @StaticMetamodel(Generos.class)
 public class Generos_ { 
 
--- DbCRUD/src/dbcrud/DbCRUD.java
+++ DbCRUD/src/dbcrud/DbCRUD.java
@@ -14,8 +14,8 @@ public class DbCRUD {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         // TODO code application logic here
-    }
+    //}
     
 }
